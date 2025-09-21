1d6e8aad02e72fd3609d86a807210edfa0b21b58
wsetiono <vincentius.william.setiono@gmail.com>
2025-09-21 11:08:52 +0700

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,10 +92,15 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="//vercel.com" />
         <link rel="dns-prefetch" href="//blob.vercel-storage.com" />
         <link rel="preload" href="/modern-software-development-team-working-on-comput.jpg" as="image" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        {/* <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.jpg" type="image/svg+xml" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.jpg" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" /> */}
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
